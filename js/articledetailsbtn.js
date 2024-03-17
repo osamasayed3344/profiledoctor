72595b76a12b08246c3a08aef1d28cdabab6399d
osamasayed3344 <135232287+osamasayed3344@users.noreply.github.com>
2024-03-17 17:10:20 +0200

    Delete js directory

--- js/articledetailsbtn.js
+++
@@ -1,7 +0,0 @@
-const articledetailsbtn=document.querySelectorAll(".bodypage .list_article .item button");
-
-for(i=0; i<articledetailsbtn.length; i++){
-    articledetailsbtn[i].onclick=function(){
-        window.open("articledetails.html");
-    }
-}
--- js/loading.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){
-    document.getElementById("loading").style.display="none";
-},5000);
